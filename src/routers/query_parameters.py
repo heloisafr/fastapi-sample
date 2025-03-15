a65dd0f74cf4667bebc80225acf5c372aa12689d
Heloisa <heloisaferr@gmail.com>
2025-03-15 19:03:03 -0300

    More samples for query parameters

--- src/routers/query_parameters.py
+++ src/routers/query_parameters.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from typing import Annotated
 
 router = APIRouter()
 
@@ -6,27 +7,17 @@ items_db = [{"item_name": "One"}, {"item_name": "Two"}, {"item_name": "Tree"}, {
 
 
 @router.get("/items/")
-def read_item(skip: int = 0, limit: int = 10):
+def read_items(skip: int = 0, limit: int = 10):
     """
-    Query parameter sample with default value
+    Query parameter (skip and limit) with default value
     """
     return items_db[skip: skip + limit]
 
 
-@router.get("/things/{item_id}")
-def read_things(item_id: int, q: str | None = None):
-    """
-    Query parameter sample with optional parameter (q)
-    q: Union[str, None], quer dizer que "q" pode ser uma string ou None
-    Se retirar o "= None" do "q" quebra documentação
-    """
-    return {"item_id": item_id, "q": q}
-
-
 @router.get("/artifacts/{item_id}")
-def read_things(item_id: int, short_description: bool = False):
+def read_artifacts(item_id: int, short_description: bool = False):
     """
-    Query parameter sample with default boolean parameter
+    Query parameter (short_description) with default boolean parameter
     """
     item = {"item_id": item_id, "description": "This is a long description"}
     if short_description:
@@ -34,10 +25,49 @@ def read_things(item_id: int, short_description: bool = False):
     return item
 
 
+@router.get("/things/{item_id}")
+def read_things(item_id: int, q: str | None = None):
+    """
+    Optional query parameter (q)
+    q: str | None means that "q" can be a string or None
+    Se retirar o "= None" do "q" quebra documentação
+    """
+    return {"item_id": item_id, "q": q}
+
+
+@router.get("/things")
+def read_things(q: Annotated[ str | None, Query(min_length=4, max_length=9, pattern="something")] = None):
+    """
+    Optional query parameter (q) with min_length, max_length and pattern validation
+    Whenever "q" is provided its length cannot exceed 10 and must have at least 4 characters
+    q: str | None means that "q" can be a string or None
+    """
+    return {"q": q}
+
+
 @router.get("/devices/{item_id}")
 def read_devices(item_id: int, model: str):
     """
-    Query parameter sample with required parameter
+    Query parameter (model) with required parameter
+    Even sending model=None it is valid
     """
     return {"item_id": item_id, "model": model}
+
+
+@router.get("/devices")
+def read_devices(model: Annotated[str, Query(min_length=4)]):
+    """
+    Query parameter (model) with required parameter and min_length validation
+    """
+    return {"model": model}
+
+
+@router.get("/devices/{item_id}/{serial}")
+def read_devices(item_id: int, serial: int, model: Annotated[str | None, Query(min_length=5)]):
+    """
+    Query parameter (model) with required parameter and min_length validation
+    Even though required can me None
+    (There was no different between this and the previous function in test cases)
+    """
+    return {"item_id": item_id, "serial": serial, "model": model}
 
--- tests/test_query_parameters.py
+++ tests/test_query_parameters.py
@@ -22,7 +22,7 @@ def test_query_parameters_read_items_ok(parameters, expected_length) -> None:
     assert len(response.json()) == expected_length
 
 
-def test_query_parameters_read_things_none_ok() -> None:
+def test_query_parameters_get_thing_none_ok() -> None:
     """
     Test query parameter with optional parameter, sending none
     """
@@ -34,7 +34,7 @@ def test_query_parameters_read_things_none_ok() -> None:
     assert response.json()["q"] is None
 
 
-def test_query_parameters_read_things_something_ok() -> None:
+def test_query_parameters_get_thing_ok() -> None:
     """
     Test query parameter with optional parameter, sending some value
     """
@@ -57,7 +57,7 @@ def test_query_parameters_read_things_something_ok() -> None:
         ("", "long"),
     ]
 )
-def test_query_parameters_read_artifacts_ok(parameters, expected_description) -> None:
+def test_query_parameters_get_artifact_ok(parameters, expected_description) -> None:
     """
     Test query parameters with default boolean value
     """
@@ -67,19 +67,52 @@ def test_query_parameters_read_artifacts_ok(parameters, expected_description) ->
     assert expected_description in response.json()["description"]
 
 
-def test_query_parameters_read_devices_ok() -> None:
+def test_query_parameters_read_stuffs_something_ok() -> None:
+    """
+    Test query parameter with optional parameter with min_length, max_length and pattern validation
+    """
+    response = client.get("/things/?q=something")
+    assert response.status_code == 200
+    assert "q" in response.json()
+    assert "something" == response.json()["q"]
+
+
+@pytest.mark.parametrize(
+    "q, expected_error",
+    [
+        ("somethings", "String should have at most 9 characters"),
+        ("new", "String should have at least 4 characters"),
+        ("thing", "String should match pattern"),
+    ]
+)
+def test_query_parameters_read_stuffs_something_error(q, expected_error) -> None:
+    """
+    Test query parameter with optional parameter with min_length, max_length and pattern validation
+    """
+    response = client.get(f"/things/?q={q}")
+    assert response.status_code == 422
+    assert expected_error in response.text
+
+
+@pytest.mark.parametrize(
+    "model, expected_model", [
+        (None, ""),
+        ("something", "something")
+    ]
+)
+def test_query_parameters_required_ok(model, expected_model) -> None:
     """
     Test query parameter with required parameter
     """
-    response = client.get("/devices/1/?model=something")
+    response = client.get("/devices/1/", params={"model":model})
     assert response.status_code == 200
     assert "item_id" in response.json()
     assert "model" in response.json()
     assert 1 == response.json()["item_id"]
-    assert "something" == response.json()["model"]
+    assert expected_model == response.json()["model"]
 
 
-def test_query_parameters_read_devices_error_missing_value() -> None:
+def test_query_parameters_required_error() -> None:
     """
     Test query parameter with required parameter
     """
@@ -87,3 +120,45 @@ def test_query_parameters_read_devices_error_missing_value() -> None:
     assert response.status_code == 422
     assert "Field required" in response.text
 
+
+def test_query_parameters_required_with_validation_ok() -> None:
+    """
+    Test query parameter with required parameter and min_length validation
+    """
+    response = client.get("/devices", params={"model": "something"})
+    assert response.status_code == 200
+    assert "model" in response.json()
+    assert "something" == response.json()["model"]
+
+
+@pytest.mark.parametrize(
+    "params, expected_error", [
+        (None, "Field required"),
+        ({"model": None}, "String should have at least 4 characters"),
+        ({"model": "new"}, "String should have at least 4 characters")
+    ]
+)
+def test_query_parameters_required_with_validation_error(params, expected_error) -> None:
+    """
+    Test query parameter with required parameter and min_length validation
+    """
+    response = client.get("/devices", params=params)
+    assert response.status_code == 422
+    assert expected_error in response.text
+
+
+@pytest.mark.parametrize(
+    "params, expected_error", [
+        (None, "Field required"),
+        ({"model": None}, "String should have at least 5 characters"),
+        ({"model": "new"}, "String should have at least 5 characters")
+    ]
+)
+def test_query_parameters_required_can_be_none_with_validation_error(params, expected_error) -> None:
+    """
+    Test query parameter with required parameter and min_length validation
+    Even though required could be None
+    """
+    response = client.get("/devices/1/123/", params=params)
+    assert response.status_code == 422
+    assert expected_error in response.text
